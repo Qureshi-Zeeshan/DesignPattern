076fa3a5aaa03f9314e0bd4ae18fc787e7981623
Qureshi-Zeeshan <zeeshanqureshi7167@gmail.com>
2025-04-30 12:14:43 +0530

    Initial commit

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DesignPattern
+{
+    class DesignPattern
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
