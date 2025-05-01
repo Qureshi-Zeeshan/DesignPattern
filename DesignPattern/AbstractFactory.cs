4cafb63db42c2764c7a39bcc7afcb8b66c6fd904
Qureshi-Zeeshan <zeeshanqureshi7167@gmail.com>
2025-05-01 12:44:52 +0530

    Factory abstract

---
+++ DesignPattern/AbstractFactory.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    interface Icar
+    {
+        void car();
+    }
+    interface Ibike
+    {
+        void bike();
+    }
+    class HondaCar : Icar
+    {
+       public void car()
+        {
+            Console.WriteLine("This is honda car");
+        }
+
+    }
+    class HondaBike : Ibike
+    {
+        public void bike()
+        {
+            Console.WriteLine("This is honda bike");
+        }
+    }
+    class SuzukiCar : Icar
+    {
+       public void car()
+        {
+            Console.WriteLine("This is Suzuki car");
+        }
+
+    }
+    class SuzukiBike : Ibike
+    {
+        public void bike()
+        {
+            Console.WriteLine("This is Suzuki bike");
+        }
+    }
+    interface Get_Vehicle
+    {
+        Icar GetCar();
+        Ibike GetBike();
+    }
+    class Honda : Get_Vehicle
+    {
+        public Ibike GetBike()
+        {
+            return new HondaBike();
+        }
+
+        public Icar GetCar()
+        {
+            return new HondaCar();
+        }
+    }
+    class Suzuki : Get_Vehicle
+    {
+        public Ibike GetBike()
+        {
+            return new SuzukiBike();
+        }
+
+        public Icar GetCar()
+        {
+            return new SuzukiCar();
+        }
+    }
+
+    internal class AbstractFactory
+    {
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -8,6 +8,18 @@ namespace DesignPattern
             SingletonDemo obj =  SingletonDemo.get_singleton();
             SingletonDemo obj1 = SingletonDemo.get_singleton();
 
+            Vehicle_Info bike = factory.vehicle_factory("bike");
+            bike.seater();
+            bike.milage();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Get_Vehicle honda = new Honda();
+            Ibike honda_bike =honda.GetBike();
+            honda_bike.bike();
+
         }
     }
 }
