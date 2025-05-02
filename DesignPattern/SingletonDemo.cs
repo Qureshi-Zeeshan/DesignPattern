a7ed49af2eb6cceae96c1edbc2040635a7fce98e
Qureshi-Zeeshan <zeeshanqureshi7167@gmail.com>
2025-05-02 19:38:13 +0530

    using lazy in singleton

--- DesignPattern/SingletonDemo.cs
+++ DesignPattern/SingletonDemo.cs
@@ -8,14 +8,26 @@ namespace DesignPattern
 
         private SingletonDemo() { }
 
+        private static object obj=new object();
+
         public static SingletonDemo get_singleton()
         {
+            lock (obj)
+            {
             if(sinstance == null)
             {
                 sinstance = new SingletonDemo();
             }
+
+            }
             return sinstance;
         }
 
+    }
+    internal sealed class lazyloading
+    {
+        private static readonly Lazy<lazyloading> instance=new Lazy<lazyloading>(() => new lazyloading());
+        private lazyloading() { }
+        public static lazyloading Instance => instance.Value;
     }
 }
